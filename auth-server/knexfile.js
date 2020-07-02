f35bab2ab08131fed45e4ec99e4b055f8fc02cc1
Mithilesh Kohale <beingtmk@gmail.com>
2020-07-02 23:00:26 +0530

    db fix

--- auth-server/knexfile.js
+++ auth-server/knexfile.js
@@ -3,19 +3,19 @@
 const databaseName = "postgres";
 const pg = require('pg');
 
-const connection_url = process.env.DATABASE_URL || `postgres://postgres:@localhost:5432/${databaseName}`;
-// const dbConfig = {
-//   user: 'postgres',
-//   password: 'ultimate',
-//   database: 'postgres',
-//   host: '127.0.0.1',
-//   port: '3306'
-//  };
+// const connection_url = process.env.DATABASE_URL || `postgres://postgres:@localhost:5432/${databaseName}`;
+const dbConfig = {
+  user: 'postgres',
+  password: 'ultimate',
+  database: 'postgres',
+  host: '127.0.0.1',
+  port: '3306'
+ };
 
 
 module.exports = {
   client: 'pg',
-  connection: connection_url,
+  connection: dbConfig,
   migrations: {
     directory: __dirname + '/db/migrations'
   }
